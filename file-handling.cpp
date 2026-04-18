9a2dac5597405d712487102f3833d9ee498042c0
yzevaughn <ty202500492@wmsu.edu.ph>
2026-04-18 16:08:30 +0800

    Add files via upload

---
+++ file-handling.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    ofstream file("color.txt");
+
+    if (file.is_open())
+    {
+        file << "Red\n";
+        file << "Green\n";
+        file << "Blue\n";
+        file << "Yellow\n";
+        file << "Pink\n";
+        file.close();
+    }
+    else
+    {
+        cout << "Unable to open file";
+    }
+
+    ifstream infile("color.txt");
+    ofstream copyFile("color_copy.txt");
+
+    if (copyFile.is_open() && infile.is_open())
+    {
+        string line;
+
+        while (getline(infile, line))
+        {
+            copyFile << line << endl;
+        }
+        cout << "File copied successfully." << endl;
+        copyFile.close();
+        infile.close();
+    }
+    else
+    {
+        cout << "Unable to open file";
+    }
+
+    return 0;
+}
